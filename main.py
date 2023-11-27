be6b35eb8d87b209069fb4c43247bda711422ab9
Sebastian Gummerer <sebastian.gummerer@gmail.com>
2023-11-27 18:18:41 +0100

    clear

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-import time
-import tkinter as tk
-from src.Display import Display
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    # removes the windows title bar CHECK ON LINUX!!!
-    #root.overrideredirect(True)
-    root.resizable(0,0)
-    app = Display(root)
-    root.mainloop()
+'''
+main file
+'''
