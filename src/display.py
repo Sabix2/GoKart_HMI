7b7f2e65571604828a78b4a76c02102a5a0900b2
Sabix <sebastian.gummerer@gmail.com>
2024-02-27 17:01:01 +0100

    removed unnessecary code

--- src/display.py
+++ src/display.py
@@ -26,21 +26,4 @@ class Display:
         self.battery.update()
         self.revolutions.update()
         self.error.update()
-        self.close_Button()
         self.power.update()
-
-    # function to draw the button to close the window on the top right
-    def close_Button(self):
-        # draw the button
-        pygame.draw.rect(self.screen, (255, 0, 0), (760, 0, 40, 40))
-        # draw the x
-        pygame.draw.line(self.screen, (0, 0, 0), (770, 10), (790, 30), 5)
-        pygame.draw.line(self.screen, (0, 0, 0), (770, 30), (790, 10), 5)
-        # check if the mouse is over the button
-        mouse = pygame.mouse.get_pos()
-        if mouse[0] > 760 and mouse[0] < 800 and mouse[1] > 0 and mouse[1] < 40:
-            # check if the button is clicked
-            if pygame.mouse.get_pressed()[0] == 1:
-                # close the window
-                pygame.quit()
-                sys.exit()
